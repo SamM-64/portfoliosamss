9361abf600ef4f932e07dfff2c590bd83b787e50
Samantha Morata <sam@icap.fr>
2023-12-01 08:15:05 +0100

    fix: noindex, nofollow

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 var positions = [
-    { id: "admin",male: "Apprenti administrateur cloud", female: "Apprentie administratrice cloud"}
+    { id: "admin", female: "Apprentie administratrice cloud"}
 ];
 
 //Cursor
-var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function(e) {
-  cursor.style.left = e.clientX + "px",
-    cursor.style.top = e.clientY + "px";
-});
+// var cursor = document.getElementById("cursor");
+// document.body.addEventListener("mousemove", function(e) {
+//   cursor.style.left = e.clientX + "px",
+//     cursor.style.top = e.clientY + "px";
+// });
 
 
 function init(){
@@ -15,12 +15,12 @@ function init(){
     var user = {
         name: { first: "Samantha", last: "Morata" },
         picture: { large: "https://media.licdn.com/dms/image/C4E03AQHzQuBzf1xP2w/profile-displayphoto-shrink_800_800/0/1655208276384?e=1704931200&v=beta&t=XPbNYBrE4dk2nvIn_X5T7ZldfgjK-GKIn2Hf2gYVdxI" },
-        gender: "female",  // ou "female" selon le cas
-        position: positions.find(pos => pos.id === "admin"),  // Remplacez "front" par l'id de la position souhaitée
-        company: "Icap",  // Remplacez par le nom de l'entreprise si nécessaire
-        location: { city: "Toulouse", country: "France" },  // Remplacez par la ville et le pays souhaités
-        speciality: "l'informatique",  // Remplacez par la spécialité souhaitée
-        email: "smorata@icap.fr"  // Remplacez par l'e-mail souhaité
+        gender: "female", 
+        position: positions.find(pos => pos.id === "admin"),  
+        company: "Icap",  
+        location: { city: "Toulouse", country: "France" },  
+        speciality: "l'informatique",  
+        email: "sam@icap.fr"  
     };
 
     renderFromUser(user);
@@ -32,7 +32,7 @@ function renderFromUser(user) {
     document.querySelector("#person-firstname").innerHTML = user.name.first;
     document.querySelector("#person-job").innerHTML = user.position[user.gender];
     document.querySelector("#person-position").innerHTML = user.position[user.gender];
-    document.querySelector("#person-company").innerHTML = user.company ? " chez " + user.company : "en recherche d'emploi";
+    document.querySelector("#person-company").innerHTML = user.company ? " chez " + user.company : "en poste";
     document.querySelector("#person-city").innerHTML = user.location.city + ", " + user.location.country;
     document.querySelector("#person-speciality").innerHTML = user.speciality;
     document.querySelector("#person-speciality").innerHTML = user.speciality;
